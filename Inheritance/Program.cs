626643044aae4ecf244d96bcbb7ccf7923aab089
Robin Mauritz <robinmauritzrm@gmail.com>
2020-08-07 21:32:28 +0700

    Initial commit

---
+++ Inheritance/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace Inheritance
+{
+    //* Ga pake Getter Setter ama semua variabel dibikin public biar simpel
+    class Person
+    {
+        public string Name;
+    }
+
+    class Employee : Person
+    {
+        public string Department;
+
+        public Employee(string name, string department)
+        {
+            Name = name;
+            Department = department;
+        }
+
+        public string GetInfo()
+        {
+            return string.Format("The employee name is {0} which is in {1} department", Name, Department);
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee someone = new Employee("Rogan", "Programmer");
+
+            Console.WriteLine(someone.GetInfo());
+        }
+    }
+}
